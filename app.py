bf04e46e42b730813ae542ba30a84c19864e1ceb
sunmoonhaze <surajdhere0000@gmail.com>
2024-10-28 22:57:47 +0530

    Updated requirements.txt with cleaned package names and versions

--- app.py
+++ app.py
@@ -1,12 +1,49 @@
 from flask import Flask, render_template, request, send_from_directory
 import pickle
 import numpy as np
-import project_1
-import project_2
+
 
 app = Flask(__name__)
 
+#############################################################################################
+# Cypher Encoder Decoder
 
+def caesar(original_text, shift_amount, choice):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    cypher_text = ""
+    for i in original_text:
+        if i not in alphabet:
+            cypher_text += i
+        else:
+            if choice == "encrypt":
+                cypher_text += alphabet[(alphabet.index(i) + shift_amount) % 26]
+            elif choice == "decrypt":
+                cypher_text += alphabet[(alphabet.index(i) - shift_amount) % 26]
+    return cypher_text
+
+#######################################################################################################
+
+# AL wire Rod Predictor
+
+from flask import Flask, render_template, request, send_from_directory
+import pickle
+import numpy as np
+
+
+
+
+# Define the relative path to your pickle file
+# PICKLE_FILE_PATH = "projectFiles/project2/data/Al_wire_rod.sav"  # Update with your project name
+PICKLE_FILE_PATH = "notebooks/project2/Al_wire_rod.sav"
+
+
+def load_model():
+    # Load the machine learning model
+    with open(PICKLE_FILE_PATH, 'rb') as f:
+        model = pickle.load(f)
+    return model
+
+#####################################################################################################
 
 @app.route('/')
 def home():
@@ -20,13 +57,13 @@ def Project1():
         shift = int(request.form["shift"])
         choice = request.form["choice"]
         print(f"Received: text={text}, shift={shift}, choice={choice}")
-        result = project_1.caesar(text, shift, choice)
+        result = caesar(text, shift, choice)
     return render_template("project1/index1.html", result=result)
 
 @app.route('/Project2')
-def project_2():
+def project2():
     # Load the model when needed
-    model = project_2.load_model()
+    model = load_model()
     # You can now use the model for predictions or other tasks
     # Serve the index2.html file from the static folder
     return send_from_directory("templates/project2/", "index2_0.html")
@@ -34,7 +71,7 @@ def project_2():
 @app.route('/predict', methods=['POST'])
 def predict():
     # Load the model when needed
-    model = project_2.load_model()
+    model = load_model()
     # You can now use the model for predictions or other tasks
     
     # Get form data
