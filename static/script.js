aee3647d61deb794d32cd23200478935057f0776
sunmoonhaze <surajdhere0000@gmail.com>
2024-10-28 06:10:33 +0530

    Updated requirements.txt with cleaned package names and versions

--- static/script.js
+++ static/script.js
@@ -14,7 +14,7 @@ const projects = [
         ]
     },
     {
-        name: "Cipher Encoder Decoder",
+        name: "Cypher Encoder Decoder",
         links: [
             { label: "Version 1.0", url: "https://cypher-game.onrender.com/Project1" }
         ]
